6278c20a55a5ab4a1a52eba98a7f5f0c5fbf41d1
zjouahri <49081437+ziko199@users.noreply.github.com>
2025-01-16 20:27:48 +0100

    create the model PaymentRequest and PaymentResponse

---
+++ webshop/src/main/java/de/frankfurtuas/cloud/webshop/Paymentservice/dto/PaymentRequest.java
@@ -0,0 +1,31 @@
+package de.frankfurtuas.cloud.webshop.Paymentservice.dto;
+
+public class PaymentRequest {
+
+    private String paymentMethod;
+
+    private double amount;
+
+    public PaymentRequest() {}
+
+    public PaymentRequest(String paymentMethod, double amount) {
+        this.paymentMethod = paymentMethod;
+        this.amount = amount;
+    }
+
+    public String getPaymentMethod() {
+        return paymentMethod;
+    }
+
+    public void setPaymentMethod(String paymentMethod) {
+        this.paymentMethod = paymentMethod;
+    }
+
+    public double getAmount() {
+        return amount;
+    }
+
+    public void setAmount(double amount) {
+        this.amount = amount;
+    }
+}
