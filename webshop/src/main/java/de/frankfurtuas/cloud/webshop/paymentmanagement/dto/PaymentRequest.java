dfab5747ec6fbb752d777efaef84753e76302c57
zjouahri <49081437+ziko199@users.noreply.github.com>
2025-02-02 18:40:57 +0100

    refactoring

--- webshop/src/main/java/de/frankfurtuas/cloud/webshop/paymentmanagement/dto/PaymentRequest.java
+++ webshop/src/main/java/de/frankfurtuas/cloud/webshop/paymentmanagement/dto/PaymentRequest.java
@@ -1,31 +1,17 @@
 package de.frankfurtuas.cloud.webshop.paymentmanagement.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class PaymentRequest {
 
     private String paymentMethod;
 
     private double amount;
-
-    public PaymentRequest() {}
-
-    public PaymentRequest(String paymentMethod, double amount) {
-        this.paymentMethod = paymentMethod;
-        this.amount = amount;
-    }
-
-    public String getPaymentMethod() {
-        return paymentMethod;
-    }
-
-    public void setPaymentMethod(String paymentMethod) {
-        this.paymentMethod = paymentMethod;
-    }
-
-    public double getAmount() {
-        return amount;
-    }
-
-    public void setAmount(double amount) {
-        this.amount = amount;
-    }
 }
