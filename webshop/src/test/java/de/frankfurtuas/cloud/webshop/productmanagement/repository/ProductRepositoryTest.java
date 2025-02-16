7085b58deb349088ebddb88aabffb99658b64922
zjouahri <49081437+ziko199@users.noreply.github.com>
2025-02-16 18:04:19 +0100

    add tests

--- webshop/src/test/java/de/frankfurtuas/cloud/webshop/productmanagement/repository/ProductRepositoryTest.java
+++ webshop/src/test/java/de/frankfurtuas/cloud/webshop/productmanagement/repository/ProductRepositoryTest.java
@@ -6,6 +6,12 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.test.context.TestPropertySource;
 
+import java.math.BigDecimal;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
 @DataJpaTest
 @TestPropertySource(locations = "classpath:application-test.properties")
 class ProductRepositoryTest {
@@ -15,94 +21,87 @@ class ProductRepositoryTest {
 
     @Test
     void testCreateAndFindByIdProduct() {
-        //        // Arrange
-        //        Product product = new Product();
-        //        product.setId(2L);
-        //        product.setName("Laptop");
-        //        product.setDescription("High-end gaming laptop");
-        //        product.setPrice(1500.0);
-        //
-        //        productRepository.save(product);
-        //
-        //        // Act
-        //        Optional<Product> findProductById = productRepository.findById(1L);
-        //
-        //        // Assert
-        //        assertThat(findProductById).isPresent();
-        //        assertThat(findProductById.get().getId()).isEqualTo(2L);
-        //        assertThat(findProductById.get().getName()).isEqualTo("Laptop");
-        //        assertThat(findProductById.get().getDescription()).isEqualTo("High-end gaming laptop");
-        //        assertThat(findProductById.get().getPrice()).isEqualTo(1500.0);
+        // Arrange
+        Product product = new Product();
+        product.setName("Laptop");
+        product.setDescription("High-end gaming laptop");
+        product.setPrice(BigDecimal.valueOf(1500.0));
+
+        productRepository.save(product);
+
+        // Act
+        Optional<Product> findProductById = productRepository.findById(1L);
+
+        // Assert
+        assertThat(findProductById).isPresent();
+        assertThat(findProductById.get().getId()).isEqualTo(1L);
+        assertThat(findProductById.get().getName()).isEqualTo("Laptop");
+        assertThat(findProductById.get().getDescription()).isEqualTo("High-end gaming laptop");
+        assertThat(findProductById.get().getPrice()).isEqualTo(BigDecimal.valueOf(1500.0));
     }
 
     @Test
     void testCreateAndFindAllProducts() {
-        //        // Arrange
-        //        Product product1 = new Product();
-        //        product1.setId(1L);
-        //        product1.setName("Laptop");
-        //        product1.setDescription("High-end gaming laptop");
-        //        product1.setPrice(1500.0);
-        //
-        //        Product product2 = new Product();
-        //        product2.setId(2L);
-        //        product2.setName("Xbox");
-        //        product2.setDescription("With Fifa 25");
-        //        product2.setPrice(500.0);
-        //
-        //        productRepository.save(product1);
-        //        productRepository.save(product2);
-        //
-        //        // Act
-        //        List<Product> products = productRepository.findAll();
-        //
-        //        // Assert
-        //        assertThat(products).hasSize(2);
-        //        assertThat(products.get(0).getId()).isEqualTo(1L);
-        //        assertThat(products.get(0).getName()).isEqualTo("Laptop");
-        //        assertThat(products.get(0).getDescription()).isEqualTo("High-end gaming laptop");
-        //        assertThat(products.get(0).getPrice()).isEqualTo(1500.0);
-        //        assertThat(products.get(1).getId()).isEqualTo(2L);
-        //        assertThat(products.get(1).getName()).isEqualTo("Xbox");
-        //        assertThat(products.get(1).getDescription()).isEqualTo("With Fifa 25");
-        //        assertThat(products.get(1).getPrice()).isEqualTo(500.0);
+        // Arrange
+        Product product1 = new Product();
+        product1.setName("Laptop");
+        product1.setDescription("High-end gaming laptop");
+        product1.setPrice(BigDecimal.valueOf(1500.0));
+
+        Product product2 = new Product();
+        product2.setName("Xbox");
+        product2.setDescription("With Fifa 25");
+        product2.setPrice(BigDecimal.valueOf(500.0));
+
+        productRepository.save(product1);
+        productRepository.save(product2);
+
+        // Act
+        List<Product> products = productRepository.findAll();
+
+        // Assert
+        assertThat(products).hasSize(2);
+        assertThat(products.get(0).getName()).isEqualTo("Laptop");
+        assertThat(products.get(0).getDescription()).isEqualTo("High-end gaming laptop");
+        assertThat(products.get(0).getPrice()).isEqualTo(BigDecimal.valueOf(1500.0));
+        assertThat(products.get(1).getName()).isEqualTo("Xbox");
+        assertThat(products.get(1).getDescription()).isEqualTo("With Fifa 25");
+        assertThat(products.get(1).getPrice()).isEqualTo(BigDecimal.valueOf(500.0));
     }
 
     @Test
     void testUpdateProduct() {
-        //        // Arrange
-        //        Product product = new Product();
-        //        product.setId(1L);
-        //        product.setName("Laptop");
-        //        product.setDescription("High-end gaming laptop");
-        //        product.setPrice(1500.0);
-        //
-        //        // Act
-        //        Product savedProduct = productRepository.save(product);
-        //        savedProduct.setPrice(500.0);
-        //        List<Product> products = productRepository.findAll();
-        //
-        //        // Assert
-        //        assertThat(products).hasSize(1);
-        //        assertThat(products.get(0).getPrice()).isEqualTo(500.0);
+        // Arrange
+        Product product = new Product();
+        product.setName("Laptop");
+        product.setDescription("High-end gaming laptop");
+        product.setPrice(BigDecimal.valueOf(1500.0));
+
+        // Act
+        Product savedProduct = productRepository.save(product);
+        savedProduct.setPrice(BigDecimal.valueOf(500.0));
+        List<Product> products = productRepository.findAll();
+
+        // Assert
+        assertThat(products).hasSize(1);
+        assertThat(products.get(0).getPrice()).isEqualTo(BigDecimal.valueOf(500.0));
     }
 
     @Test
     void testDeleteProduct() {
         // Arrange
         Product product = new Product();
-        //        product.setId(1L);
-        //        product.setName("Keyboard");
-        //        product.setDescription("Mechanical keyboard");
-        //        product.setPrice(120.0);
-        //
-        //        Product savedProduct = productRepository.save(product);
-        //
-        //        // Act
-        //        productRepository.delete(savedProduct);
-        //        List<Product> remainingProducts = productRepository.findAll();
-        //
-        //        // Assert
-        //        assertThat(remainingProducts).isEmpty();
+        product.setName("Keyboard");
+        product.setDescription("Mechanical keyboard");
+        product.setPrice(BigDecimal.valueOf(120.0));
+
+        Product savedProduct = productRepository.save(product);
+
+        // Act
+        productRepository.delete(savedProduct);
+        List<Product> remainingProducts = productRepository.findAll();
+
+        // Assert
+        assertThat(remainingProducts).isEmpty();
     }
 }
--- webshop/src/test/java/de/frankfurtuas/cloud/webshop/productmanagement/service/ProductServiceTest.java
+++ webshop/src/test/java/de/frankfurtuas/cloud/webshop/productmanagement/service/ProductServiceTest.java
@@ -1,5 +1,6 @@
 package de.frankfurtuas.cloud.webshop.productmanagement.service;
 
+import de.frankfurtuas.cloud.webshop.productmanagement.exception.ProductNotFoundException;
 import de.frankfurtuas.cloud.webshop.productmanagement.model.Product;
 import de.frankfurtuas.cloud.webshop.productmanagement.repository.ProductRepository;
 import org.junit.jupiter.api.BeforeEach;
@@ -9,12 +10,15 @@ import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.test.context.TestPropertySource;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.any;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
@@ -40,7 +44,7 @@ class ProductServiceTest {
         Product product = new Product();
         product.setName("Laptop");
         product.setDescription("Gaming Laptop");
-        product.setPrice(1500.0);
+        product.setPrice(BigDecimal.valueOf(1500.0));
 
         when(productRepository.save(any(Product.class))).thenReturn(product);
 
@@ -61,7 +65,7 @@ class ProductServiceTest {
         product.setId(1L);
         product.setName("Phone");
         product.setDescription("Smartphone");
-        product.setPrice(999.0);
+        product.setPrice(BigDecimal.valueOf(999.0));
 
         when(productRepository.findById(1L)).thenReturn(Optional.of(product));
 
@@ -75,17 +79,61 @@ class ProductServiceTest {
     }
 
     @Test
+    void testGetProductByName() {
+        // Arrange
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Phone");
+        product.setDescription("Smartphone");
+        product.setPrice(BigDecimal.valueOf(999.0));
+
+        when(productRepository.findByName("Phone")).thenReturn(List.of(product));
+
+        // Act
+        List<Product> foundProduct = productService.searchProductsByName("Phone");
+
+        // Assert
+        assertNotNull(foundProduct);
+        assertThat(foundProduct).hasSize(1);
+        assertThat(foundProduct.get(0).getName()).isEqualTo("Phone");
+        verify(productRepository, times(1)).findByName("Phone");
+    }
+
+    @Test
+    void testGetProductByCategory() {
+        // Arrange
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Phone");
+        product.setDescription("Smartphone");
+        product.setCategory("Electronics");
+        product.setPrice(BigDecimal.valueOf(999.0));
+
+        when(productRepository.findByCategory("Electronics")).thenReturn(List.of(product));
+
+        // Act
+        List<Product> foundProduct = productService.getProductsByCategory("Electronics");
+
+        // Assert
+        assertNotNull(foundProduct);
+        assertThat(foundProduct).hasSize(1);
+        assertThat(foundProduct.get(0).getName()).isEqualTo("Phone");
+        assertThat(foundProduct.get(0).getCategory()).isEqualTo("Electronics");
+        verify(productRepository, times(1)).findByCategory("Electronics");
+    }
+
+    @Test
     void testFindAllProducts() {
         // Arrange
         Product product1 = new Product();
         product1.setName("Tablet");
         product1.setDescription("Portable device");
-        product1.setPrice(499.0);
+        product1.setPrice(BigDecimal.valueOf(499.0));
 
         Product product2 = new Product();
         product2.setName("Monitor");
         product2.setDescription("4K Display");
-        product2.setPrice(300.0);
+        product2.setPrice(BigDecimal.valueOf(300.0));
 
         List<Product> products = Arrays.asList(product1, product2);
         when(productRepository.findAll()).thenReturn(products);
@@ -101,11 +149,45 @@ class ProductServiceTest {
 
     @Test
     void testUpdateProduct() {
-        // still
+        // Arrange
+        Product product = new Product();
+        product.setId(1L);
+        product.setName("Laptop");
+        product.setDescription("Gaming Laptop");
+        product.setPrice(BigDecimal.valueOf(1500.0));
+
+        Product updatedProduct = new Product();
+        updatedProduct.setId(1L);
+        updatedProduct.setName("Laptop");
+        updatedProduct.setDescription("Gaming Laptop");
+        updatedProduct.setPrice(BigDecimal.valueOf(500.0));
+
+        when(productRepository.findById(1L)).thenReturn(Optional.of(product));
+        when(productRepository.save(updatedProduct)).thenReturn(updatedProduct);
+
+        // Act
+        Product savedProduct = productService.updateProduct(1L, updatedProduct);
+
+        // Assert
+        assertThat(savedProduct).isNotNull();
+        assertThat(savedProduct.getPrice()).isEqualTo(BigDecimal.valueOf(500.0));
+        assertThat(savedProduct.getName()).isEqualTo("Laptop");
+        assertThat(savedProduct.getDescription()).isEqualTo("Gaming Laptop");
+        verify(productRepository, times(1)).findById(1L);
+        verify(productRepository, times(1)).save(updatedProduct);
     }
 
     @Test
-    void testDeleteProduct() {
-        // still
+    void testDeleteProduct_ThrowsProductNotFoundException() {
+        // Arrange
+        when(productRepository.findById(1L)).thenReturn(Optional.empty());
+
+        // Act and Assert
+        ProductNotFoundException exception = assertThrows(ProductNotFoundException.class, () -> {
+            productService.deleteProduct(1L);
+        });
+
+        // Assert
+        assertEquals("Product with id 1 not found", exception.getMessage());
     }
 }
